8b61cd59ff0f51224acd010f7a0f7909950c0809
edwardfgthomas@gmail.com <edwardfgthomas@gmail.com>
2023-01-11 13:02:03 +0000

    finish sub_strings code using .scan and regex

--- sub_strings.rb
+++ sub_strings.rb
@@ -1,5 +1,14 @@
 # Implement a method #substrings that takes a word as the first argument and then an array of valid substrings (your dictionary) as the second argument. It should return a hash listing each substring (case insensitive) that was found in the original string and how many times it was found.
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
-def substrings(word, array)
-
+def substrings(string, dictionary)
+  matches = {}
+  dictionary.each do |word|
+    if string.downcase.include? word
+      matches[word] = string.downcase.scan(/(?=#{word})/).count
+    end
+  end
+  matches
 end
+
+puts substrings("Howdy partner, sit down! How's it going?", dictionary)
